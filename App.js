a591067948314a78b5612bddaab00b9b41ad2cb6
lisa ewart <lnemedium@gmail.com>
2017-09-06 16:06:29 -0400

    app.js change

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ export default class App extends React.Component {
       
       <View style={styles.container}>
          
-
+        <SignIn />
         <MapRender style={styles.container} />  
       </View>
       
